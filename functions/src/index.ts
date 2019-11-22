f16df18b95d6394cce9c3b12f3491e0619408203
a5vTech <a5vgaminghq@gmail.com>
2019-11-22 13:05:04 +0100

    add trigger for delete department

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,12 +6,6 @@ admin.initializeApp();
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const updateDepartment = functions.firestore.document('departments/{depId}').onUpdate(( change, context ) => {
-    // admin.firestore().collection('users').add({tester: 'ff'}).then(( result ) => {
-    //     console.log('success' + result + ':::::: ' + change.after.data());
-    // }).catch(( err ) => {
-    //     console.log(err)
-    // });
-
     // @ts-ignore
     if ( change.before.data().name !== change.after.data().name ) {
         const userRef = admin.firestore().collection('users');
@@ -31,6 +25,36 @@ export const updateDepartment = functions.firestore.document('departments/{depId
             console.log('Error getting documents', err);
         });
     }
-
-
 });
+
+
+export const deleteDepartment = functions.firestore.document('departments/{depId}').onDelete(( change, context ) => {
+    const userRef = admin.firestore().collection('users');
+    userRef.get().then(( snapshot ) => {
+        snapshot.forEach(doc => {
+            console.log('Delete department', '=>', change.data(), change.id);
+            // @ts-ignore
+            if ( doc.data().department === change.data().name ) {
+                // Delete user from Auth
+                admin.auth().deleteUser(doc.id)
+                    .then(function () {
+                        console.log('Successfully deleted user from AUTH');
+                        // Delete user from database
+                        admin.firestore().collection('users').doc(doc.id).delete().then(( res ) => {
+                            console.log('Successfully deleted user from database', doc.id);
+                        }).catch(( err ) => {
+                            console.log(err);
+                        });
+                    })
+                    .catch(function ( error ) {
+                        console.log('Error deleting user:', error);
+                    });
+                console.log('Detected user with same department', doc.id)
+            }
+
+        })
+    }).catch(( err ) => {
+        console.log(err);
+    })
+});
+

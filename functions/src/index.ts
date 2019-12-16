5b5321a0d921e1e64d1c67535e573d66ebb497ab
a5vTech <a5vgaminghq@gmail.com>
2019-12-16 10:25:59 +0100

    Working with password Update!

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -38,24 +38,22 @@ export const deleteDepartment = functions.firestore.document('departments/{depId
     })
 });
 
-export const passwordChange = functions.https.onCall(( data, context ) => {
+export const passwordChange = functions.https.onCall(async ( data, context ) => {
     console.log('Password change!');
     console.log('DATA RECEIVED: ', data);
     console.log('CONTEXT RECEIVED: ', context);
-    if (!context.auth) {
+    if ( !context.auth ) {
         // Throwing an HttpsError so that the client gets the error details.
         throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
             'while authenticated.');
     }
-    admin.auth().updateUser(data.uid, {password: data.pass}).then(( result ) => {
+    return admin.auth().updateUser(data.uid, {password: data.pass}).then(( result ) => {
         console.log('User password has been updated => ', data.uid);
         return {success: 'true'};
     }).catch(( err ) => {
         console.log('User password has NOT been updated => ', data.uid, '===>> ', err);
         return {success: 'false'};
     });
-
-
     // return {text: 'OK'};
 });
 
